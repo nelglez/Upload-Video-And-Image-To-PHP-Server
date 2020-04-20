f9ea3bcace90d006c68c9dd8d8e900fda2ca5b92
Nelson Gonzalez <nelglezfl@gmail.com>
2020-04-20 10:59:58 -0400

    Initial Commit

---
+++ UploadToServer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UploadToServer
+//
+//  Created by Nelson Gonzalez on 4/20/20.
+//  Copyright © 2020 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
